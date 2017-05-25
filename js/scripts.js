14392fa944983208b3b8fab1dfd6ac3e3265aae7
mackenziekellogg <kellogg_21@hotmail.com>
2017-05-25 14:37:11 -0700

    css fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-// Scripts
+// scripts.js
+// Store Banner Images
+var img0 = 'https://unsplash.it/600/300?image=1';
+var img1 = 'https://unsplash.it/600/300?image=2';
+var img2 = 'https://unsplash.it/600/300?image=3';
+
+// Random Number Generator
+var maxRand = 3;
+var randNum = Math.floor( Math.random() * maxRand );
+console.log(randNum);
+
+// Swap Out Banner
+$('header img').attr('src', eval('img' + randNum) );
+
 // Light Gallery
 $('figure').lightGallery({
     selector: 'a',
